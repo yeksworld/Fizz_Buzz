8a837f9d72fe61a371db647f8d154d21b47323af
yunus <yunusemre4634@gmail.com>
2021-10-27 13:22:54 +0300

    Initial commit

---
+++ Fizz.py
@@ -0,0 +1,12 @@
+total = 0
+for number in range(1, 101):
+
+    if number % 5 == 0 and number % 3 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0 and number % 5 != 0:
+         print("Fizz")
+    elif number % 5 == 0 and number % 3 != 0:
+         print("Buzz")
+    else:
+        total = number
+        print(total)
